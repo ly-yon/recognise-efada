1b62ac40deeda169f6f579923bf7a748225f9158
Hamza Abdulla <hamza.abdulla1123@gmail.com>
2024-08-04 12:13:36 +0300

    On branch push Initial commit Changes to be committed:     new file:   .gitignore 	new file:   Test/Test (1).png 	new file:   Test/Test (10).jpg 	new file:   Test/Test (2).png 	new file:   Test/Test (3).png 	new file:   Test/Test (4).png 	new file:   Test/Test (5).png 	new file:   Test/Test (6).jpeg 	new file:   Test/Test (6).jpg 	new file:   Test/Test (7).jpg 	new file:   Test/Test (8).jpg 	new file:   Test/Test (9).jpg 	new file:   Train/efada/Train (1).png 	new file:   Train/efada/Train (10).png 	new file:   Train/efada/Train (11).png 	new file:   Train/efada/Train (12).png 	new file:   Train/efada/Train (13).png 	new file:   Train/efada/Train (14).png 	new file:   Train/efada/Train (15).png 	new file:   Train/efada/Train (16).png 	new file:   Train/efada/Train (17).png 	new file:   Train/efada/Train (18).png 	new file:   Train/efada/Train (19).png 	new file:   Train/efada/Train (2).png 	new file:   Train/efada/Train (20).png 	new file:   Train/efada/Train (3).png 	new file:   Train/efada/Train (4).png 	new file:   Train/efada/Train (5).png 	new file:   Train/efada/Train (6).png 	new file:   Train/efada/Train (7).png 	new file:   Train/efada/Train (8).png 	new file:   Train/efada/Train (9).png 	new file:   Train/not_efada/Train (1).jpg 	new file:   Train/not_efada/Train (1).png 	new file:   Train/not_efada/Train (10).jpg 	new file:   Train/not_efada/Train (11).jpg 	new file:   Train/not_efada/Train (12).jpg 	new file:   Train/not_efada/Train (2).jpg 	new file:   Train/not_efada/Train (2).png 	new file:   Train/not_efada/Train (20).jpg 	new file:   Train/not_efada/Train (3).jpg 	new file:   Train/not_efada/Train (3).png 	new file:   Train/not_efada/Train (4).jpg 	new file:   Train/not_efada/Train (4).png 	new file:   Train/not_efada/Train (5).jpg 	new file:   Train/not_efada/Train (5).png 	new file:   Train/not_efada/Train (6).jpg 	new file:   Train/not_efada/Train (6).png 	new file:   Train/not_efada/Train (7).jpg 	new file:   Train/not_efada/Train (7).png 	new file:   Train/not_efada/Train (8).jpg 	new file:   Train/not_efada/Train (9).jpg 	new file:   data.js 	new file:   index.js 	new file:   model.js 	new file:   model/model.json 	new file:   model/weights.bin 	new file:   package.json 	new file:   predict.js

---
+++ data.js
@@ -0,0 +1,71 @@
+const tf = require("@tensorflow/tfjs-node");
+const fs = require("fs");
+const path = require("path");
+const TRAIN_IMAGES_DIR = "./train";
+const TEST_IMAGES_DIR = "./test";
+// Classes that I will train
+const classes = ["efada", "not_efada"];
+//Total number of classes
+const n = 2;
+function loadImages(dataDir) {
+  const images = [];
+  const labels = [];
+
+  for (let j = 0; j < n; j++) {
+    var dataDir1 = dataDir + `/${classes[j]}`;
+    var files = fs.readdirSync(dataDir1);
+    for (let i = 0; i < files.length; i++) {
+      //   console.log(`Checking ${classes[j]} images..|| image = ${files[i]}`);
+      //   if (!files[i].toLocaleLowerCase().endsWith(".jpg")) {
+      //     continue;
+      //   }
+      console.log(`Checking ${classes[j]} images..|| image = ${files[i]}`);
+      var filePath = path.join(dataDir1, files[i]);
+
+      var buffer = fs.readFileSync(filePath);
+      var imageTensor = tf.node
+        .decodeImage(buffer, 3)
+        .resizeNearestNeighbor([224, 224])
+        .toFloat()
+        .div(tf.scalar(255.0))
+        .expandDims();
+      console.log(imageTensor.shape);
+      images.push(imageTensor);
+      labels.push(j);
+    }
+  }
+  return [images, labels];
+}
+// Helper class to handle loading training and test data.
+class FoodDataset {
+  constructor() {
+    this.trainData = [];
+    this.testData = [];
+    this.n = n;
+  }
+  // Loads training and test data.
+  loadData() {
+    console.log("Loading images...");
+    this.trainData = loadImages(TRAIN_IMAGES_DIR);
+    console.log(this.trainData);
+    this.testData = loadImages(TEST_IMAGES_DIR);
+    console.log("Images loaded successfully.");
+  }
+  getTrainData() {
+    return {
+      images: tf.concat(this.trainData[0]),
+      labels: tf
+        .oneHot(tf.tensor1d(this.trainData[1], "int32"), this.n)
+        .toFloat(),
+    };
+  }
+  getTestData() {
+    return {
+      images: tf.concat(this.testData[0]),
+      labels: tf
+        .oneHot(tf.tensor1d(this.testData[1], "int32"), this.n)
+        .toFloat(),
+    };
+  }
+}
+module.exports = new FoodDataset();
---
+++ predict.js
@@ -0,0 +1,44 @@
+const tf = require("@tensorflow/tfjs-node");
+const fs = require("fs");
+async function main(name) {
+  try {
+    model = await tf.loadLayersModel("file://model/model.json");
+    const image = fs.readFileSync(`Test/${name}`);
+    const classes = ["efada", "not_efada"];
+    TARGET_CLASSES = Object.assign({}, classes);
+    // Pre-process the image
+    let tensor = tf.node
+      .decodeImage(image, 3)
+      .resizeNearestNeighbor([224, 224])
+      .toFloat()
+      .div(tf.scalar(255.0))
+      .expandDims();
+    let predictions = await model.predict(tensor).data();
+    let top5 = Array.from(predictions)
+      .map(function (p, i) {
+        return {
+          probability: p,
+          className: TARGET_CLASSES[i], // we are selecting the value from the obj
+        };
+      })
+      .sort((a, b) => b.probability - a.probability);
+    // console.log(top5);
+    console.log(
+      "After Concluding the Result:-\nPicture " +
+        name +
+        " is: " +
+        top5[0].className
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function read(path) {
+  let files = fs.readdirSync(path).map((fileName) => {
+    console.log(fileName);
+
+    return fileName;
+  });
+  for (i in files) await main(files[i]);
+}
+read("Test");
